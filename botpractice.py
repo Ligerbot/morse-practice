de617310e829dd8088ad6c2b8b075e41bc431c88
Ligerbot <ilan@snackmail.cc>
2026-06-16 12:27:33 -0700

    More commands in botpractice.py

--- botpractice.py
+++ botpractice.py
@@ -4,6 +4,47 @@ import pymorsecode.pymorsecode
 import ollama
 conversation = []
 conversation.append({"role": "system", "content": "You are a morse code practice bot. Your callsign is KO6BOT. All answers must be short answers in ENGLISH. The user will send you dots and dashes. Decode those yourself and then reply with a short text sentence NOT IN MORSE CODE. Only use common characters, no periods or commands, etc. Your replies must be short and concise, using only common words. Do not use common ham radio morse code terms like QSL unless the user sends them to you. It CANNOT BE RANDOM CHARACTERS. REPLY WITH WORDS. NO ABBREVIATIONS. Do not ever use dashes or emojis. You don't need to echo what the user says. Instead, try to have a conversation with the user. You are only allowed to answer in lowercase letters. No punctuation marks of any kind. No apostrophes either"})
+cheatsheet = [
+"a = .-",
+"b = -...",
+"c = -.-.",
+"d = -..",
+"e = .",
+"f = ..-.",
+"g = --.",
+"h = ....",
+"i = ..",
+"j = .---",
+"k = -.-",
+"l = .-..",
+"m = --",
+"n = -.",
+"o = ---",
+"p = .--.",
+"q = --.-",
+"r = .-.",
+"s = ...",
+"t = -",
+"u = ..-",
+"v = ...-",
+"w = .--",
+"x = -..-",
+"y = -.--",
+"z = --..",
+"1 = .----",
+"2 = ..---",
+"3 = ...--",
+"4 = ....-",
+"5 = .....",
+"6 = -....",
+"7 = --...",
+"8 = ---..",
+"9 = ----.",
+"0 = -----",
+". = .-.-.-",
+", = --..--",
+"? = ..--.."
+]
 def llamaHandler(text):
 	global conversation
 	client = ollama.Client()
@@ -24,7 +65,18 @@ while True:
 	if text == "/show":
 		print(output)
 	elif text == "/help":
-		print("Available commands: /show (shows previous message from bot), /exit (exits the program gracefully (ctrl+c works too!))")
+		print("Morse code chatbot practice")
+		print("An LLM will reply to any morse code message entered with morse code so you can practice decoding morse.")
+		print("Available commands: /show (shows previous message from bot), /exit (exits the program gracefully (ctrl+c works too!)), /cheat (prints a cheat sheet for morse code), /context (prints out the whole entire conversation so far)")
+	elif text == "/cheat":
+		print("Morse code cheat sheet")
+		for character in cheatsheet:
+			print(character)
+	elif text == "/context":
+		print("Conversation so far:")
+		for a in conversation:
+			print(str(a))
+#		print(str(conversation))
 	elif text == "/exit":
 		print("Thanks for using my morse code trainer program")
 		exit(0)
