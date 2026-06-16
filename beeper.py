eca4fbb9da30e0c1469276a28df30f9c1d0e0bc1
Ligerbot <ilan@snackmail.cc>
2026-06-15 21:58:23 -0700

    Now sends callsign first when program is started

--- beeper.py
+++ beeper.py
@@ -1,6 +1,8 @@
 import pymorsecode.pymorsecode
 callsign = input("Callsign: ")
 wpm = input("WPM (5-25): ")
+morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=20)
+morse.play_morse()
 morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=int(wpm))
 while True:
 	text = input("> ")
