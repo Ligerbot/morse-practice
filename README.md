3f551e1fd1638abbf4c3564aa9757a4dd96243f5
Ligerbot <ilan@snackmail.cc>
2026-06-15 22:13:40 -0700

    Uploading progress for the night

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Choose two different modes, one where you type dashes and dots and the program converts them, or one where you hold or tap space bar for variable length dashes and dots.
 
 `python3 beeper.py` runs the converter from dots and dashes to sound.
+
+playsound is required, otherwise on Linux you might hear weird screeching.
+
+To add more of a gap between words just add extra spaces.
+
+Also to enable or disable the auto id at start check in the top of beeper.pi for `autoid = True`
--- beeper.py
+++ beeper.py
@@ -1,9 +1,18 @@
+from playsound import playsound
 import pymorsecode.pymorsecode
-callsign = input("Callsign: ")
-wpm = input("WPM (5-25): ")
-morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=20)
-morse.play_morse()
+
+#feel free to change this
+autoid = False
+
+if autoid:
+	callsign = input("Callsign: ")
+	wpm = input("WPM (5-25): ")
+	morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=20)
+	morse.play_morse()
+else:
+	wpm = input("WPM (5-25): ")
 morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=int(wpm))
 while True:
 	text = input("> ")
-	morse.play_morse(text)
+	morse.save_wav("tmp.wav", text)
+	playsound("tmp.wav")
