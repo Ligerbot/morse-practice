078afe84cfbf93dedfbf8980a530f9fe68613035
Ligerbot <ilan@snackmail.cc>
2026-06-16 08:13:12 -0700

    Progress on the slash seperation feature

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Morse practice
 
-Choose two different modes, one where you type dashes and dots and the program converts them, or one where you hold or tap space bar for variable length dashes and dots.
+Choose two different modes, one where you type dashes and dots and the program converts them, or one where you hold or tap space bar for variable length dashes and dots (second one not added yet).
 
 `python3 beeper.py` runs the converter from dots and dashes to sound.
 
 playsound is required, otherwise on Linux you might hear weird screeching.
 
-To add more of a gap between words just add extra spaces.
+To add more of a gap between words just add extra spaces or look in the top of beeper.py for settings.
 
 Also to enable or disable the auto id at start check in the top of beeper.pi for `autoid = True`
--- beeper.py
+++ beeper.py
@@ -2,17 +2,25 @@ from playsound import playsound
 import pymorsecode.pymorsecode
 
 #feel free to change this
-autoid = False
+autoid = False #if using the ham radio bands then you can use this to automatically send your callsign before transmitting. a callsign is not required for this software though (morse code isn't restricted to ham radio)
+extraspacing = True #this option is recommended if you are learning
+
+#TODO: fix the following feature:
+slashseperation = False #seperate letters with a space and words with a / and no space if this is on
 
 if autoid:
 	callsign = input("Callsign: ")
 	wpm = input("WPM (5-25): ")
-	morse = pymorsecode.pymorsecode.MorseCode(callsign, wpm=20)
+	morse = pymorsecode.pymorsecode.MorseCode(callsign + " is learning cw", wpm=20)
 	morse.play_morse()
 else:
 	wpm = input("WPM (5-25): ")
 	morse = pymorsecode.pymorsecode.MorseCode("a", wpm=int(wpm))
 while True:
 	text = input("> ")
+	if slashseperation:
+		text.replace("/", "  ")
+	if extraspacing and not slashseperation:
+		text.replace(" ", "  ")
 	morse.save_wav("tmp.wav", text)
 	playsound("tmp.wav")
